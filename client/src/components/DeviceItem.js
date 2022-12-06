1811470dba32d43c414c5281172214130826528e
Ilya <ilyxa@gmail.com>
2022-12-06 18:50:52 +0200

    First

---
+++ client/src/components/DeviceItem.js
@@ -0,0 +1,24 @@
+import {Card, Col} from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+//import star from '../assets/star.png'
+import {useNavigate} from "react-router-dom"
+import {DEVICE_ROUTE} from "../utils/consts";
+
+
+const DeviceItem = ({device}) => {
+    const navigate = useNavigate()
+
+    return (
+        <Col md={3} className={"mt-3"} onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)}>
+            <Card style={{width: 150, cursor: 'pointer'}} border={"light"}>
+                <Image width={150} height={150} src={process.env.REACT_APP_API_URL + device.img}/>
+                <div className="text-black-50 mt-1 d-flex justify-content-between align-items-center">
+                    <div>{device.name}</div>
+                    <div>{device.price}</div>
+                </div>
+            </Card>
+        </Col>
+    );
+};
+
+export default DeviceItem;
---
+++ client/src/pages/DevicePage.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from 'react';
+import {Button, Card, Col, Container, Image, Row, Form} from "react-bootstrap";
+import {useParams} from 'react-router-dom'
+import {fetchOneDevice} from "../http/deviceAPI";
+
+const DevicePage = () => {
+    const [device, setDevice] = useState({info: []})
+    const {id} = useParams()
+    useEffect(() => {
+        fetchOneDevice(id).then(data => setDevice(data))
+        // eslint-disable-next-line
+    }, [])
+    
+    return (
+        <Container className="mt-3">
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={process.env.REACT_APP_API_URL + device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Form className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                    </Form>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
+                    >
+                        <h3>От: {device.price} грн.</h3>
+                        <Button variant={"outline-dark"}>Оплатить</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Характеристики</h1>
+                {device.info.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
+    );
+};
+
+export default DevicePage;
